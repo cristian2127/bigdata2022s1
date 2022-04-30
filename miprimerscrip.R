f78214b92e1a4140615f782e526238a00c530096
ISanchezA <102835640+cristian2127@users.noreply.github.com>
2022-04-29 21:02:01 -0400

    se creo mi primer script

---
+++ miprimerscrip.R
@@ -0,0 +1,29 @@
+
+#suma
+4+4
+#multiplicacion
+3*5
+#division
+4/6
+#exponencial
+2**3
+
+#raiz
+4**1/2
+
+
+# se guarda en respuesta la operacion 
+
+#4+4
+respuesta <- 4+4
+
+
+#muestro la informacion que uno decide
+print(respuesta)
+
+
+#almacenando texto el texto va en comillas
+
+respuesta <- "que tengan bonito descanso"
+print(respuesta)
+
