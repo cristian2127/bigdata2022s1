765bebfd37b0bc3a3d0e24413732d923517f074b
ISanchezA <102835640+cristian2127@users.noreply.github.com>
2022-06-17 20:59:19 -0400

    for up

--- miprimerscrip.R
+++ miprimerscrip.R
@@ -1,3 +1,22 @@
+pathmipc <-"~/GitHub/bigdata2022s1"
+setwd(pathmipc)
+
+install.packages("sys")
+library(sys)
+
+divisiondedosnumeros <- 3/5
+
+a <- 3
+b <- 5
+sumadedosnumeros <- a + b 
+
+variableboleana <- TRUE
+variableboleana <- FALSE
+
+vareableboleana <- a > b
+vareableboleana <- a < b
+vareableboleana <- a > b
+
 
 #suma
 4+4
@@ -26,4 +45,69 @@ print(respuesta)
 
 respuesta <- "que tengan bonito descanso"
 print(respuesta)
+
+
+
+#modulo
+4 %% 2 == 0
+3450%%999
+
+
+######################################## CONDICIONALES#######################################
+
+a <- 0
+ 
+   
+if(a == 0){
+    print("a es cero")
+ }else if (a %% 2 == 0) {
+  print("a numero es par")
+}else{
+  print("a numero es impar")
+}
+
+
+
+############# listas##############################
+
+var_list <- list("Andres",1,TRUE,"Ana")
+print(var_list)[[2]]
+
+len_var_list <- length(var_list)
+
+var_list <- c(var_list, "hols")
+
+print(var_list[3])
+
+
+
+###########################################forlup#################################
+
+
+
+for(i in 1:10) {
+  print("=========")
+  print(i)
+  Sys.sleep(5)
+  print(".")
+  Sys.sleep(5)
+  print(".")
+}
+
+
+
+####¿como muestro los elementos de una lista con for?
+
+var_list <- list("Andres",1,TRUE,"Ana","juan","cristian")
+
+#forma numero 1
+for( i in var_list ){
+  print(i)
+  Sys.sleep(2)
+}
+
+
+
+
+  
 
